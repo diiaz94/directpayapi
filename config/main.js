407988de292964244ab054956ba7c6d83f585c0e
Pedro Diaz <diaz2209gmail.com>
2017-11-02 11:24:23 -0400

    fix enviroments config

--- config/main.js
+++ config/main.js
@@ -1,19 +1,19 @@
 var config = {
-    'production': {
-        'database': 'mongodb://localhost:27017/directpay',
-        'port': '3000',
-        'host': 'http://0.0.0.0',
-        'secret': 'B7syiTVp15hE9yvIUd8560sf0aQzM8qjTKPphTqX3JrQxV9ivtndyU5ZKY1P4Jfm',
-        'dropboxKey': 'i_dx6w9mm_YAAAAAAAABRTnjkpZNmfhGoRtYeqx_kFvMQZqW_ZJFh5WeTXNKyQLu'
-    },
-    'development': {
-        'database': 'mongodb://admin:123456@ds155934.mlab.com:55934/directpay',
-        'port': '80',
-        'host': 'http://0.0.0.0',
-        'secret': 'B7syiTVp15hE9yvIUd8560sf0aQzM8qjTKPphTqX3JrQxV9ivtndyU5ZKY1P4Jfm',
-        'dropboxKey': 'i_dx6w9mm_YAAAAAAAABRTnjkpZNmfhGoRtYeqx_kFvMQZqW_ZJFh5WeTXNKyQLu'
-    }
+  'development': {
+    'database': 'mongodb://localhost:27017/directpay',
+    'port': '3000',
+    'host': 'http://0.0.0.0',
+    'secret': 'B7syiTVp15hE9yvIUd8560sf0aQzM8qjTKPphTqX3JrQxV9ivtndyU5ZKY1P4Jfm',
+    'dropboxKey': 'i_dx6w9mm_YAAAAAAAABRTnjkpZNmfhGoRtYeqx_kFvMQZqW_ZJFh5WeTXNKyQLu'
+  },
+  'production': {
+    'database': 'mongodb://admin:123456@ds155934.mlab.com:55934/directpay',
+    'port': '80',
+    'host': 'http://0.0.0.0',
+    'secret': 'B7syiTVp15hE9yvIUd8560sf0aQzM8qjTKPphTqX3JrQxV9ivtndyU5ZKY1P4Jfm',
+    'dropboxKey': 'i_dx6w9mm_YAAAAAAAABRTnjkpZNmfhGoRtYeqx_kFvMQZqW_ZJFh5WeTXNKyQLu'
+  }
 }
 exports.get = function(env) {
-    return config[env] || config.development;
+  return config[env] || config.development;
 }
